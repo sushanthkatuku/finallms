895f960ab13d71dc99af73b7eda44aff9be812a1
sushanthkatuku <59360951+sushanthkatuku@users.noreply.github.com>
2024-05-07 01:04:47 +0530

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'librarymanagement.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+from django.contrib.auth import get_user_model
+def reset_password(u, password):
+    try:
+        user = get_user_model().objects.get(username=u)
+    except:
+        return "User could not be found"
+    user.set_password(password)
+    user.save()
+    return "Password has been changed successfully"
+
+if __name__ == '__main__':
+    reset_password('admin@gmail.com','admin')
+    main()
